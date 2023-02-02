6affcc6a5e620a853cb8dda6e6a1527a52331d60
Gleb Abramov <abramov.gleb@gmail.com>
2023-02-02 23:32:18 +0100

    Fix it

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,4 +1,11 @@
-public record Student( int id, String name, String zipCode, int age ) {
+import org.jetbrains.annotations.Contract;
 
+import java.util.Optional;
+
+public record Student(int id, String name, String zipCode, int age ) {
+
+	protected static Optional<Student> empty() {
+		return Optional.empty();
+	}
 }
 
